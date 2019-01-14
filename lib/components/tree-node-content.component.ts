7db60489852e249430d45e818d2029f8854972cc
asoyka-di <46683430+asoyka-di@users.noreply.github.com>
2019-01-14 11:24:51 -0500

    Update tree-node-content.component.ts
    Added img tag for ne_icon

--- lib/components/tree-node-content.component.ts
+++ lib/components/tree-node-content.component.ts
@@ -5,6 +5,7 @@ import { TreeNode } from '../models/tree-node.model';
   selector: 'tree-node-content',
   encapsulation: ViewEncapsulation.None,
   template: `
+  <img class="ne_icon" style="width: 24px; height: 24px; position: relative; bottom: 1px; right: 4px;" *ngIf="node.data.hasOwnProperty('icon')" [src]="node.icon" />
   <span *ngIf="!template">{{ node.displayField }}</span>
   <ng-container
     [ngTemplateOutlet]="template"
